6d73e4da6c77918a8554ea72f4b291d0b9f6f1bf
Kai Builder <vankhoa0603@gmail.com>
2026-01-16 12:39:03 +0700

    test rollback 2

--- main.go
+++ main.go
@@ -116,7 +116,7 @@ const indexTemplate = `<!DOCTYPE html>
 <body>
     <header>
         <h1>{{ .title }}</h1>
-        <p>Only public variables are shown here - Test rollback 1. Private server values stay on the backend. - test new branch</p>
+        <p>Only public variables are shown here - Test rollback 2. Private server values stay on the backend. - test new branch</p>
     </header>
     <main>
         <section>
